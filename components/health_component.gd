bf3db192f268337f442e8329a5eba21b8e7aa692
TheSmileyDroid <ghsilha@gmail.com>
2024-12-06 14:50:01 -0300

    feat: Adiciona sistema de animação e refatora componentes de saúde e estratégia de ataque

--- components/health_component.gd
+++ components/health_component.gd
@@ -3,8 +3,15 @@ class_name HealthComponent
 
 var stats: CharacterStats
 
+var animation_player: AnimationPlayer
+
+func _ready():
+    animation_player = get_parent().get_node("AnimationPlayer")
+
 func take_damage(amount: int):
     stats.health -= amount
+    if animation_player:
+        animation_player.play("hit")
     if stats.health <= 0:
         die()
 
