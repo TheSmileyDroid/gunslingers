d682f13c9501744807b10e17b63ed571c81a705c
Gabriel H. Silva <ghsilha@gmail.com>
2025-01-28 21:33:49 -0300

    feat: ajusta atributos do inimigo Bomb Rat e melhora verificação de componentes de saúde na explosão

--- scenes/props/explosion.gd
+++ scenes/props/explosion.gd
@@ -8,12 +8,16 @@ var max_time_alive := 0.5
 var team: Character.Team = Character.Team.ENEMY
 @onready var area2d :Area2D = get_node("Area2D")
 @onready var shape : CircleShape2D = get_node("Area2D").get_node("CollisionShape2D").shape
+@onready var particles: GPUParticles2D = $GPUParticles2D
 
 func _ready() -> void:
+	if not area2d or not shape or not particles:
+		push_error("Required nodes are missing!")
+		return
 	area2d.area_entered.connect(_on_area_entered)
 	shape.radius = 0
-	$GPUParticles2D.lifetime = size / 200.0
-	$GPUParticles2D.emitting = true
+	particles.lifetime = size / 200.0
+	particles.emitting = true
 
 	var tween = get_tree().create_tween()
 	tween.tween_property(shape, "radius", size, max_time_alive).set_trans(Tween.TRANS_BOUNCE)
@@ -22,5 +26,6 @@ func _ready() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Character:
 		var character: Character = area.get_parent() as Character
-		if character.team != team:
-			character.get_node("HealthComponent").take_damage(damage, team)
+		var health_component = character.get_node_or_null("HealthComponent")
+		if health_component and character.team != team:
+			health_component.take_damage(damage, team)
